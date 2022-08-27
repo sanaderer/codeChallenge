e2af5d8637fca9310efdbcfa43b9e3f4266d0113
sanaderer <sabrinanaderer@hotmail.com>
2022-08-27 14:01:49 -0300

    tabelas banco de dados

--- autenticacao/models.py
+++ autenticacao/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    nome = models.Charfield('Nome', max_length=150)
+    cpf = models.ForeignKey('CPF', max_length=20)
+    data_nascimento = models.DateField('Data de Nascimento')
+    telefone = models.CharField('Telefone', max_length=15)
+    email = models.EmailField('Email', max_length=255, null=True, blank=True)
+
+class Veiculo(models.Model):
+    modelo = models.CharField('Modelo', max_length=150),
+    marca = models.CharField('Marca', max_length=20)
+    ano = models.IntegerField()
+    cor = models.CharField(max_length=50)
+    placa = models.CharField(max_length=7)
+
