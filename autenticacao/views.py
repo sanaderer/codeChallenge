ddce6b70c2685f6d6876c203d33e38aa241fa465
weblerson <zonejr4@gmail.com>
2022-08-27 13:32:14 -0400

    match

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def cadastro(request):
-    match request.method:
-        case "GET":
-            pass
+    if request.method == 'GET':
+        pass
