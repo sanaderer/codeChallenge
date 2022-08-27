26db1071ac836c4303ba0d6968767961595ea6dd
sanaderer <sabrinanaderer@hotmail.com>
2022-08-27 14:50:58 -0300

    admin.py

--- autenticacao/admin.py
+++ autenticacao/admin.py
@@ -2,3 +2,9 @@ from django.contrib import admin
 from .models import Cliente, Veiculo, Produto, Servico
 
 # Register your models here.
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'cpf', 'data_nascimento', 'telefone', 'email', 'numero_protocolo']
+    list_filter = ['nome', 'cpf']
+    search_fields = ['nome', 'protocolo']
+
--- autenticacao/models.py
+++ autenticacao/models.py
@@ -9,6 +9,9 @@ class Cliente(models.Model):
     email = models.EmailField('Email', max_length=255, null=True, blank=True)
     numero_protocolo = models.CharField('Protocolo', max_length=8)
 
+    def __str__(self):
+        return self.nome
+
 class Veiculo(models.Model):
     modelo = models.CharField('Modelo', max_length=150)
     marca = models.CharField('Marca', max_length=20)
@@ -17,16 +20,26 @@ class Veiculo(models.Model):
     placa = models.CharField(max_length=7)
     pessoa = models.ForeignKey(Cliente, on_delete=models.DO_NOTHING)
 
+    def __str__(self):
+        return self.placa
+
 class Produto(models.Model):
     nome_produto = models.CharField('Produto', max_length=500)
     preco = models.FloatField()
     descricao = models.TextField()
     pessoa = models.ForeignKey(Cliente, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome_produto
     
 class Servico(models.Model):
     nome_servico = models.CharField('Servico', max_length=500)
     preco = models.FloatField()
     descricao = models.TextField()
     pessoa = models.ForeignKey(Cliente, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome_servico
+    
 
 
