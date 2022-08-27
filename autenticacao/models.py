2e82ce54fc3043a17541ef0b8a385acf7074b3b1
sanaderer <sabrinanaderer@hotmail.com>
2022-08-27 14:11:22 -0300

    tabela db

--- autenticacao/models.py
+++ autenticacao/models.py
@@ -9,9 +9,20 @@ class Cliente(models.Model):
     email = models.EmailField('Email', max_length=255, null=True, blank=True)
 
 class Veiculo(models.Model):
-    modelo = models.CharField('Modelo', max_length=150),
+    modelo = models.CharField('Modelo', max_length=150)
     marca = models.CharField('Marca', max_length=20)
     ano = models.IntegerField()
     cor = models.CharField(max_length=50)
     placa = models.CharField(max_length=7)
+
+class Produto(models.Model):
+    nome_produto = models.CharField('Produto', max_length=500)
+    preco = models.FloatField()
+    descricao = models.TextField()
+    
+class Servico(models.Model):
+    nome_servico = models.CharField('Servico', max_length=500)
+    preco = models.FloatField()
+    descricao = models.TextField()
+
 
