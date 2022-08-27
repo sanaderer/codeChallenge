b6f303b5616a8ece6ada323ff746adeff4c1a451
weblerson <zonejr4@gmail.com>
2022-08-27 15:38:05 -0400

    t

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('auth/cadastro/', views.cadastro, name='cadastro'),
-    path('index', views.index, name='index'),   
-    path('servicos', views.servicos, name='servicos'),
-    path('produtos', views.produtos, name='produtos')
+    path('index/', views.index, name='index'),   
+    path('servicos/', views.servicos, name='servicos'),
+    path('produtos/', views.produtos, name='produtos')
 ]
--- autenticacao/views.py
+++ autenticacao/views.py
@@ -2,10 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def cadastro(request):
-    if request.method == 'GET':
-        pass
-
 def index(request):
     return render(request, 'index.html')
     match request.method:
