8489d7042a82f4660b2d69ba64afa05a19204bf3
sanaderer <sabrinanaderer@hotmail.com>
2022-08-27 16:12:24 -0300

    admin

--- autenticacao/admin.py
+++ autenticacao/admin.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from .models import Cliente, Veiculo, Produto, Servico
 
 # Register your models here.
-#@admin.register(Cliente)
-#class ClienteAdmin(admin.ModelAdmin):
-   # list_display = ['nome', 'cpf', 'data_nascimento', 'telefone', 'email', 'numero_protocolo']
-    #list_filter = ['nome', 'cpf']
-    #search_fields = ['nome', 'protocolo']
+@admin.register(Cliente)
+class Cliente(admin.ModelAdmin):
+    list_display = ['nome', 'cpf', 'data_nascimento', 'telefone', 'email']
+    list_filter = ['nome', 'cpf']
+    search_fields = ['nome']
 
+admin.site.register(Veiculo)
+admin.site.register(Produto)
+admin.site.register(Servico)
